8349b5656b4454c5859f9bbb60e394e2043476eb
ilyadmtr <ilyadmytrenko@gmail.com>
2021-12-15 15:33:30 +0200

    added burger-menu

--- script/main.js
+++ script/main.js
@@ -6,3 +6,11 @@ $(document).ready(function(){
     infinite:false
   });
 });
+
+
+$(document).ready(function(){
+$('.header__burger').click(function(event){
+  $('.header__burger, .header__menu').toggleClass('active');
+  $('body').toggleClass('lock');
+})
+});
