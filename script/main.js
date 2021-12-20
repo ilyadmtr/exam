4db4aafcdb356e017dc7ee55fca13ffc35d0b5c2
ilyadmtr <ilyadmytrenko@gmail.com>
2021-12-21 00:34:49 +0200

    test

--- script/main.js
+++ script/main.js
@@ -1,36 +1,31 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   $('.slider_feedback').slick({
-//     arrows:true,
-//     dots:true,
-//     infinite:false
-//   });
-// });
-
-
-// $(document).ready(function(){
-// $('.header__burger').click(function(event){
-//   $('.header__burger, .header__menu').toggleClass('active');
-//   $('body').toggleClass('lock');
-// })
-// });
+  $('.slider_feedback').slick({
+    arrows:true,
+    dots:true,
+    infinite:false
+  });
+});
 
 
-
-// $(window).on('load resize', function() {
-//   if ($(window).width() < 769) {
-//     $('.slider_gallery:not(.slick-initialized)').slick({
-//       arrows:false,
-//       infinite: false
-//     });
-//     $('.item_none').remove();
-
-//   } else {
-//     $(".slider_gallery.slick-initialized").slick("unslick");
-//   }
-// });
+$(document).ready(function(){
+$('.header__burger').click(function(event){
+  $('.header__burger, .header__menu').toggleClass('active');
+  $('body').toggleClass('lock');
+})
+});
 
 
-// $(document).ready(function(){
-//   $('.num_input').mask('(00) 0000-0000');
-// });
+
+$(window).on('load resize', function() {
+  if ($(window).width() < 769) {
+    $('.slider_gallery:not(.slick-initialized)').slick({
+      arrows:false,
+      infinite: false
+    });
+    $('.item_none').remove();
+
+  } else {
+    $(".slider_gallery.slick-initialized").slick("unslick");
+  }
+});
